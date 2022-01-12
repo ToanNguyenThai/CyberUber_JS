c2711306585abc8777f9972cea9f4f83978a218c
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-01-12 15:38:14 +0700

    delete history file

--- .history/js/main_20220112142902.js
+++
@@ -1,9 +0,0 @@
-/* Get số km */
-var km = document.getElementById('soKM').value;
-
-/* Get btn tính tiền */
-var btn_tinhTien = document.getElementById('btn-tinhTien');
-
-btn_tinhTien.onclick = function () {
-    console.log(km);
-}
--- .history/js/main_20220112143209.js
+++
@@ -1,9 +0,0 @@
-/* Get số km */
-var km = document.getElementById('soKM');
-
-/* Get btn tính tiền */
-var btn_tinhTien = document.getElementById('btn-tinhTien');
-console.log(btn_tinhTien);
-btn_tinhTien.addEventListener("click", function () {
-    console.log("Hello");
-});
